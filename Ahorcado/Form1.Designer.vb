7022bfd82d59434e6f7924acb4a512245d397d7c
Cristian Hernández <chtello05@gmail.com>
2019-02-14 17:16:30 -0600

    Add files via upload

---
+++ Ahorcado/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtLetra = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NuevoToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnIniciar = New System.Windows.Forms.Button()
+        Me.btnIntentar = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblUsadas = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblRestantes = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.rhTxtPista = New System.Windows.Forms.RichTextBox()
+        Me.AxWindowsMediaPlayer2 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxWindowsMediaPlayer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtLetra
+        '
+        Me.txtLetra.Location = New System.Drawing.Point(484, 432)
+        Me.txtLetra.MaxLength = 1
+        Me.txtLetra.Name = "txtLetra"
+        Me.txtLetra.Size = New System.Drawing.Size(112, 20)
+        Me.txtLetra.TabIndex = 10
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(868, 24)
+        Me.MenuStrip1.TabIndex = 11
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoToolStripMenuItem1})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'NuevoToolStripMenuItem1
+        '
+        Me.NuevoToolStripMenuItem1.Name = "NuevoToolStripMenuItem1"
+        Me.NuevoToolStripMenuItem1.Size = New System.Drawing.Size(109, 22)
+        Me.NuevoToolStripMenuItem1.Text = "Nuevo"
+        '
+        'btnIniciar
+        '
+        Me.btnIniciar.Location = New System.Drawing.Point(29, 419)
+        Me.btnIniciar.Name = "btnIniciar"
+        Me.btnIniciar.Size = New System.Drawing.Size(109, 45)
+        Me.btnIniciar.TabIndex = 12
+        Me.btnIniciar.Text = "Iniciar Juego"
+        Me.btnIniciar.UseVisualStyleBackColor = True
+        '
+        'btnIntentar
+        '
+        Me.btnIntentar.Location = New System.Drawing.Point(602, 432)
+        Me.btnIntentar.Name = "btnIntentar"
+        Me.btnIntentar.Size = New System.Drawing.Size(109, 20)
+        Me.btnIntentar.TabIndex = 13
+        Me.btnIntentar.Text = "Intentar"
+        Me.btnIntentar.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(210, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(400, 320)
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(785, 441)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(83, 23)
+        Me.AxWindowsMediaPlayer1.TabIndex = 15
+        Me.AxWindowsMediaPlayer1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Berlin Sans FB", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(646, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(150, 26)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Letras usadas"
+        '
+        'lblUsadas
+        '
+        Me.lblUsadas.AutoSize = True
+        Me.lblUsadas.Font = New System.Drawing.Font("Berlin Sans FB", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUsadas.Location = New System.Drawing.Point(669, 70)
+        Me.lblUsadas.Name = "lblUsadas"
+        Me.lblUsadas.Size = New System.Drawing.Size(0, 18)
+        Me.lblUsadas.TabIndex = 17
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Berlin Sans FB", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(646, 183)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(192, 26)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Intentos restantes"
+        '
+        'lblRestantes
+        '
+        Me.lblRestantes.AutoSize = True
+        Me.lblRestantes.Font = New System.Drawing.Font("Berlin Sans FB", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRestantes.Location = New System.Drawing.Point(723, 209)
+        Me.lblRestantes.Name = "lblRestantes"
+        Me.lblRestantes.Size = New System.Drawing.Size(0, 23)
+        Me.lblRestantes.TabIndex = 19
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Berlin Sans FB", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(43, 36)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 23)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Pista :"
+        '
+        'rhTxtPista
+        '
+        Me.rhTxtPista.Location = New System.Drawing.Point(12, 71)
+        Me.rhTxtPista.Name = "rhTxtPista"
+        Me.rhTxtPista.ReadOnly = True
+        Me.rhTxtPista.Size = New System.Drawing.Size(155, 217)
+        Me.rhTxtPista.TabIndex = 21
+        Me.rhTxtPista.Text = ""
+        '
+        'AxWindowsMediaPlayer2
+        '
+        Me.AxWindowsMediaPlayer2.Enabled = True
+        Me.AxWindowsMediaPlayer2.Location = New System.Drawing.Point(785, 396)
+        Me.AxWindowsMediaPlayer2.Name = "AxWindowsMediaPlayer2"
+        Me.AxWindowsMediaPlayer2.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer2.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer2.Size = New System.Drawing.Size(75, 23)
+        Me.AxWindowsMediaPlayer2.TabIndex = 22
+        Me.AxWindowsMediaPlayer2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Ahorcado.My.Resources.Resources.fondo
+        Me.ClientSize = New System.Drawing.Size(868, 476)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer2)
+        Me.Controls.Add(Me.rhTxtPista)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblRestantes)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblUsadas)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnIntentar)
+        Me.Controls.Add(Me.btnIniciar)
+        Me.Controls.Add(Me.txtLetra)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Ahorcado"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxWindowsMediaPlayer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NuevoToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents btnIniciar As Button
+    Friend WithEvents txtLetra As TextBox
+    Friend WithEvents btnIntentar As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblUsadas As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblRestantes As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents rhTxtPista As RichTextBox
+    Friend WithEvents AxWindowsMediaPlayer2 As AxWMPLib.AxWindowsMediaPlayer
+End Class
---
+++ Ahorcado/Form1.vb
@@ -0,0 +1,151 @@
+﻿Public Class Form1
+    Private Sub Reiniciar()
+        Me.Refresh()
+    End Sub
+    Dim cafe As New Pen(Brushes.Brown)
+    Dim negro As New Pen(Brushes.Black)
+    Dim etiqueta As Label
+    Dim cantidad, cont, posetiqueta As Integer
+    Dim correctas As Integer
+    Dim intentos As Integer = 0
+    Dim palabra, letra, usadas As String
+    Dim palabras(15) As String
+    Dim pistas(15) As String
+
+    Private Sub NuevoToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles NuevoToolStripMenuItem1.Click
+        Application.Restart()
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtLetra.Enabled = False
+        btnIntentar.Enabled = False
+        lblRestantes.Text = "8"
+        palabras(0) = "hola"
+        palabras(1) = "temblor"
+        palabras(2) = "juniorexpress"
+        palabras(3) = "murcielago"
+        palabras(4) = "antigua"
+        palabras(5) = "crema"
+        palabras(6) = "computadora"
+        palabras(7) = "mono"
+        palabras(8) = "arbol"
+        palabras(9) = "manzana"
+        palabras(10) = "autobus"
+        palabras(11) = "mochila"
+        palabras(12) = "reloj"
+        palabras(13) = "gorra"
+        palabras(14) = "barco"
+
+        pistas(0) = "Saludo"
+        pistas(1) = "Moviento violento de las placas tectonicas"
+        pistas(2) = "Programa infantil de Disney Junior"
+        pistas(3) = "Animal mamífero de hábitos nocturnos, son alados y cuentan con la particularidad de dormir cabeza abajo"
+        pistas(4) = "Ciudad de guatemala tambien llamada Santiago de los Caballeros"
+        pistas(5) = "Sustancia alimenticia de consistencia más o menos pastosa"
+        pistas(6) = "Máquina electrónica capaz de almacenar información y tratarla automáticamente mediante operaciones matemáticas y lógicas."
+        pistas(7) = "Primates del suborden de los antropoides "
+        pistas(8) = "Planta de tronco leñoso, grueso y elevado que se ramifica a cierta altura del suelo formando la copa."
+        pistas(9) = "Fruto comestible, de forma redondeada y algo hundida por los extremos,puede ser roja,verde o amarilla"
+        pistas(10) = "Vehículo automóvil con capacidad para gran número de viajeros, destinado al transporte de pasajeros por carretera."
+        pistas(11) = "Bolsa para transportar provisiones en excursiones, viajes u otro tipo de desplazamientos."
+        pistas(12) = "Instrumento para medir el tiempo."
+        pistas(13) = "Prenda de vestir de forma redonda y lleva una visera."
+        pistas(14) = "Embarcación con el fondo cóncavo y con cubierta."
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnIntentar.Click
+        Dim dibujar As Graphics = PictureBox1.CreateGraphics
+        cafe.Width = 8.0F
+        negro.Width = 2.2F
+        Dim arreglo() = palabra.ToCharArray
+        usadas = usadas & " " & letra
+        letra = txtLetra.Text
+        If usadas.Contains(letra) Then
+            MessageBox.Show("Ya ingresaste esa letra,ingresa otra")
+            intentos = intentos - 1
+        Else
+            lblUsadas.Text = usadas & " " & letra
+        End If
+        posetiqueta = 50
+        For x As Integer = 0 To palabra.Length - 1
+            If arreglo(x) = letra Then
+                etiqueta = New Label()
+                etiqueta.Text = letra
+                etiqueta.Width = 50
+                etiqueta.Location = New Point(posetiqueta, 350)
+                Me.Controls.Add(etiqueta)
+                correctas += 1
+            End If
+            posetiqueta += 55
+        Next
+        If correctas = palabra.Length Then
+            Me.AxWindowsMediaPlayer2.URL = "C:\Users\informatica\Desktop\Ahorcado\winner.mp3"
+            ' My.Computer.Audio.Play = (My.Resources.fondo.)
+            Dim result As Integer = MessageBox.Show("¿Desea jugar de nuevo?", "¡¡Ganaste!! :)", MessageBoxButtons.YesNo)
+            If result = DialogResult.No Then
+                Application.Exit()
+            ElseIf result = DialogResult.Yes Then
+                Application.Restart()
+            End If
+        End If
+        If Not palabra.Contains(letra) Then
+            intentos = intentos + 1
+            Select Case intentos
+                Case 1
+                    dibujar.DrawLine(cafe, 0, 310, 80, 310)
+                    dibujar.DrawLine(cafe, 0, 10, 0, 310)
+                    dibujar.DrawLine(cafe, 0, 10, 100, 10)
+                    dibujar.DrawLine(cafe, 100, 10, 100, 50)
+                Case 2
+                    dibujar.DrawEllipse(negro, 75, 50, 50, 50)
+                    dibujar.DrawLine(negro, 90, 70, 90, 75)
+                    dibujar.DrawLine(negro, 110, 70, 110, 75)
+                    dibujar.DrawLine(negro, 90, 90, 110, 90)
+                Case 3
+                    dibujar.DrawLine(negro, 100, 100, 100, 240)
+                Case 4
+                    dibujar.DrawLine(negro, 100, 150, 50, 130)
+                Case 5
+                    dibujar.DrawLine(negro, 100, 150, 150, 130)
+                Case 6
+                    dibujar.DrawLine(negro, 100, 240, 80, 280)
+                Case 7
+                    dibujar.DrawLine(negro, 100, 240, 120, 280)
+                Case 8
+                    Me.AxWindowsMediaPlayer1.URL = "C:\Users\informatica\Desktop\Ahorcado\loser.mp3"
+                    Dim result As Integer = MessageBox.Show("¿Desea jugar de nuevo?", "Haz Perdido... :(", MessageBoxButtons.YesNo)
+                    If result = DialogResult.No Then
+                        Application.Exit()
+                    ElseIf result = DialogResult.Yes Then
+                        Application.Restart()
+                    End If
+            End Select
+        End If
+        lblRestantes.Text = CStr(8 - intentos)
+        txtLetra.Clear()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnIniciar.Click
+        'generar palabra aleatoria
+        Dim nPista As Integer
+        Randomize()
+        nPista = 14 * Rnd()
+        palabra = palabras(nPista)
+        rhTxtPista.Text = pistas(nPista)
+
+        'generar etiquetas   
+        cantidad = Len(palabra)
+        posetiqueta = 50
+
+        For cont = 1 To cantidad Step 1
+            etiqueta = New Label()
+            etiqueta.Text = "__"
+            etiqueta.Width = 50
+            etiqueta.Location = New Point(posetiqueta, 370)
+            Me.Controls.Add(etiqueta)
+            posetiqueta += 55
+        Next
+        btnIniciar.Enabled = False
+        btnIntentar.Enabled = True
+        txtLetra.Enabled = True
+    End Sub
+End Class
